8b22f9895d67dfdb1b074cbdd7d89e8d79a3491e
ArtMGreen <artyom5145@gmail.com>
2020-01-08 11:27:11 +0300

    What's new: - simple start screen - map (text file) with 2 different types of tiles - simple player movement system, player's animations are currently disabled

---
+++ pacman.py
@@ -0,0 +1,135 @@
+import sys
+import pygame
+
+pygame.init()
+pygame.key.set_repeat(0, 70)
+
+FPS = 60
+WIDTH = 1080
+HEIGHT = 720
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def load_image(name, color_key=None):    # загрузка всех изображений
+    image = pygame.image.load(name)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():    # начальный экран
+    intro_text = ["PACMAN"]
+    fon = pygame.transform.scale(load_image('data/fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 100)
+    text_coord = 100
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 100
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    # мини-цикл для начального экрана
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def load_level(filename):    # загрузка уровней
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    max_width = max(map(len, level_map))
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {'wall': load_image('data/wall.png'), 'empty': load_image('data/path.png')}
+player_image = load_image('data/pacman.png')
+
+tile_width = tile_height = 32
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+
+start_screen()
+
+player, level_x, level_y = generate_level(load_level('data/map.txt'))
+running = True
+direction = None
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                direction = 'left'
+            if event.key == pygame.K_RIGHT:
+                direction = 'right'
+            if event.key == pygame.K_UP:
+                direction = 'up'
+            if event.key == pygame.K_DOWN:
+                direction = 'down'
+    if direction == 'left':
+        player.rect.x -= 2
+    elif direction == 'right':
+        player.rect.x += 2
+    elif direction == 'up':
+        player.rect.y -= 2
+    elif direction == 'down':
+        player.rect.y += 2
+    screen.fill(pygame.Color(0, 0, 0))
+    tiles_group.draw(screen)
+    player_group.draw(screen)
+    pygame.display.flip()
+    clock.tick(60)
+terminate()
